b7205f0eaee73b14ff0926b8438260b796365166
al-ruhanyat <alruhanyat994@gmail.com>
2022-09-17 20:53:13 +0600

    modifying the previous commit

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import random
 
-number= random.randint(1,10)
-attempt= 1
-guess = int(input("Guess the number : "))
+number = random.randint(1,10)
+attempt=1
+
+
+guess= int(input("Enter the right number :"))
 
 while(True):
-    if(guess>number):
-        guess=int(input("Guess another number.This noe is too big:"))
+    if guess>number:
+        guess=int(input("Enter again, the number is too big:"))
         attempt+=1
-    elif(guess<number):
-        guess=int(input("Guess another number.This noe is too less:"))
+    elif guess<number:
+        guess=int(input("Enter again, the number is too less:"))
         attempt+=1
     else:
-        print(f"Thats the number! You guessed it right in {attempt} attempts")
+        print(f"Thats the the number!!! you guessed right number in {attempt} attempts")
         break
